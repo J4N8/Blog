006da4bc07bfc66438c98960013c0be7265bb0cf
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-02-23 14:20:54 +0100

    Fixed bootstrap not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,19 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("boostrap")
 
 // CSS
 import 'scss/site'
 // JS
 import('js/site')
+
 // Images
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
